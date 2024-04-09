ab916abc3a5cd1f1b86f1877779b1e66a67e6892
Mohammad Hassan <heidary0081@gmail.com>
2024-04-10 00:01:00 +0330

    Update googLeNet_inceptionV1.py

--- googLeNet_inceptionV1.py
+++ googLeNet_inceptionV1.py
@@ -3,10 +3,11 @@ from keras.layers.core import Layer
 import keras.backend as K
 import tensorflow as tf
 from keras.datasets import cifar10
+import math
 
 from keras.models import Model
 from keras.layers import Conv2D, MaxPool2D, \
-    Dropout, Dense, Input, concatenate, \
+    Dropout, Dense, Input, Concatenate, \
     GlobalAveragePooling2D, AveragePooling2D, \
     Flatten
 
@@ -22,6 +23,8 @@ from keras.callbacks import LearningRateScheduler
 
 
 def load_cifar10_data(img_rows, img_cols):
+
+    num_classes = 10
     # Load cifar10 training and validation sets
     (X_train, Y_train), (X_valid, Y_valid) = cifar10.load_data()
 
@@ -43,3 +46,169 @@ def load_cifar10_data(img_rows, img_cols):
     return X_train, Y_train, X_valid, Y_valid
 
 X_train, y_train, X_test, y_test = load_cifar10_data(224, 224)
+
+
+def inception_module(input,
+                     filters_1x1,
+                     filters_3x3_reduce,
+                     filters_3x3,
+                     filters_5x5_reduce,
+                     filters_5x5,
+                     filters_pool_proj,
+                     name=None):
+    conv_1x1 = Conv2D(filters_1x1, (1, 1), padding='same', activation='relu')(input)
+
+    conv_3x3 = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation='relu')(input)
+    conv_3x3 = Conv2D(filters_3x3, (3, 3), padding='same', activation='relu')(conv_3x3)
+
+    conv_5x5 = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation='relu')(input)
+    conv_5x5 = Conv2D(filters_5x5, (5, 5), padding='same', activation='relu')(conv_5x5)
+
+    pool_proj = MaxPool2D((3, 3), strides=(1, 1), padding='same')(input)
+    pool_proj = Conv2D(filters_pool_proj, (1, 1), padding='same', activation='relu')(pool_proj)
+
+    output = Concatenate([conv_1x1, conv_3x3, conv_5x5, pool_proj], axis=3, name=name)
+
+    return output
+
+input_layer = Input(shape=(224, 224, 3))
+
+x = Conv2D(64, (7, 7), padding='same', strides=(2, 2), activation='relu', name='conv_1_7x7/2', kernel_initializer=kernel_init, bias_initializer=bias_init)(input_layer)
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_1_3x3/2')(x)
+x = Conv2D(64, (1, 1), padding='same', strides=(1, 1), activation='relu', name='conv_2a_3x3/1')(x)
+x = Conv2D(192, (3, 3), padding='same', strides=(1, 1), activation='relu', name='conv_2b_3x3/1')(x)
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_2_3x3/2')(x)
+
+x = inception_module(x,
+                     filters_1x1=64,
+                     filters_3x3_reduce=96,
+                     filters_3x3=128,
+                     filters_5x5_reduce=16,
+                     filters_5x5=32,
+                     filters_pool_proj=32,
+                     name='inception_3a')
+
+x = inception_module(x,
+                     filters_1x1=128,
+                     filters_3x3_reduce=128,
+                     filters_3x3=192,
+                     filters_5x5_reduce=32,
+                     filters_5x5=96,
+                     filters_pool_proj=64,
+                     name='inception_3b')
+
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3/2')(x)
+
+x = inception_module(x,
+                     filters_1x1=192,
+                     filters_3x3_reduce=96,
+                     filters_3x3=208,
+                     filters_5x5_reduce=16,
+                     filters_5x5=48,
+                     filters_pool_proj=64,
+                     name='inception_4a')
+
+output_1 = AveragePooling2D((5, 5), strides=3)(x)
+output_1 = Conv2D(128, (1, 1), padding='same', activation='relu')(output_1)
+output_1 = Flatten()(output_1)
+output_1 = Dense(1024, activation='relu')(output_1)
+output_1 = Dropout(0.7)(output_1)
+output_1 = Dense(10, activation='softmax', name='output_1')(output_1)
+
+x = inception_module(x,
+                     filters_1x1=160,
+                     filters_3x3_reduce=112,
+                     filters_3x3=224,
+                     filters_5x5_reduce=24,
+                     filters_5x5=64,
+                     filters_pool_proj=64,
+                     name='inception_4b')
+
+x = inception_module(x,
+                     filters_1x1=128,
+                     filters_3x3_reduce=128,
+                     filters_3x3=256,
+                     filters_5x5_reduce=24,
+                     filters_5x5=64,
+                     filters_pool_proj=64,
+                     name='inception_4c')
+
+x = inception_module(x,
+                     filters_1x1=112,
+                     filters_3x3_reduce=144,
+                     filters_3x3=288,
+                     filters_5x5_reduce=32,
+                     filters_5x5=64,
+                     filters_pool_proj=64,
+                     name='inception_4d')
+
+output_2 = AveragePooling2D((5, 5), strides=3)(x)
+output_2 = Conv2D(128, (1, 1), padding='same', activation='relu')(output_2)
+output_2 = Flatten()(output_2)
+output_2 = Dense(1024, activation='relu')(output_2)
+output_2 = Dropout(0.7)(output_2)
+output_2 = Dense(10, activation='softmax', name='auxilliary_output_2')(output_2)
+
+x = inception_module(x,
+                     filters_1x1=256,
+                     filters_3x3_reduce=160,
+                     filters_3x3=320,
+                     filters_5x5_reduce=32,
+                     filters_5x5=128,
+                     filters_pool_proj=128,
+                     name='inception_4e')
+
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_4_3x3/2')(x)
+
+x = inception_module(x,
+                     filters_1x1=256,
+                     filters_3x3_reduce=160,
+                     filters_3x3=320,
+                     filters_5x5_reduce=32,
+                     filters_5x5=128,
+                     filters_pool_proj=128,
+                     name='inception_5a')
+
+x = inception_module(x,
+                     filters_1x1=384,
+                     filters_3x3_reduce=192,
+                     filters_3x3=384,
+                     filters_5x5_reduce=48,
+                     filters_5x5=128,
+                     filters_pool_proj=128,
+                     name='inception_5b')
+
+x = GlobalAveragePooling2D(name='avg_pool_5_3x3/1')(x)
+
+x = Dropout(0.4)(x)
+
+output_3 = Dense(10, activation='softmax', name='output')(x)
+
+model = Model(input_layer, [output_1, output_2, output_3], name='inception_v1')
+
+model.summary()
+
+epochs = 1
+initial_lrate = 0.01
+
+def decay(epoch, steps=100):
+    initial_lrate = 0.01
+    drop = 0.96
+    epochs_drop = 8
+    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    return lrate
+
+sgd = SGD(lr=initial_lrate, momentum=0.9, nesterov=False)
+
+lr_sc = LearningRateScheduler(decay, verbose=1)
+
+model.compile(loss=['categorical_crossentropy', 'categorical_crossentropy', 'categorical_crossentropy'],
+              loss_weights=[1, 0.3, 0.3],
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+history = model.fit(X_train, y= [y_train, y_train, y_train],
+                    validation_data=(X_test, [y_test, y_test, y_test]),
+                    epochs=epochs,
+                    batch_size=256,
+                    callbacks=[lr_sc])
